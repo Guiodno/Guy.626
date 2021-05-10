015413580361d2d2eebb3f0a3927c3ceb3a5326d
peterson <saintaimepeterson@gmail.com>
2021-05-10 14:26:07 -0400

    add event

--- static/index.js
+++ static/index.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    alert("hello,  peterson🤪")
+
+    document.querySelector('.alert').style.display = 'none';
+
+    document.querySelector('#submit').addEventListener('click' = ()=>{
+        if((document.querySelector("#email").value != '') && (document.querySelector('#password')).value != ''){
+            alert('connect')
+            return true
+        }else{
+            document.querySelector('.alert').style.display = 'block';
+            alert('username or password')
+            return false
+        }
+    })
+
+    //alert("hello,  peterson🤪")
 })
